b0894f1c45a55eb49bb729159fd9f1ab12803129
Andrii Danylkov <danilkovxp@gmail.com>
2023-05-17 11:17:21 +0200

    Add new hook for uploading profile images

--- BeHealth/Frontend/BeHealthFrontend/src/pages/doctor/profile/ProfileHooks.ts
+++ BeHealth/Frontend/BeHealthFrontend/src/pages/doctor/profile/ProfileHooks.ts
@@ -1,18 +1,29 @@
 import { api_path } from "../../../utils/api";
 
-export const useAddCertificate = (file: File, id: string) => {
+export const useAddCertificate = (file: File, id: string): Promise<string> => {
     return (async () => {
-        const formData = new FormData()
-        formData.append("image", file)
-        const response = await fetch(`${api_path}/api/doctors/${id}/certificates`,
-        {
-            method: 'POST',
-            body: formData,
-        });
-        const body = await response.text();
-        return `${api_path}/Images/${body}`
+        return uploadImage(file, `${api_path}/api/doctors/${id}/certificates`)
     })();
 }
+
+export const useChangeProfileImage = (file: File, id: string): Promise<string> => {
+    return (async () => {
+        return uploadImage(file, `${api_path}/api/doctors/${id}/profileImage`)
+    })();
+}
+
+const uploadImage = async (file: File, url: string): Promise<string> => {
+    const formData = new FormData()
+    formData.append("image", file)
+    const response = await fetch(url,
+    {
+        method: 'POST',
+        body: formData,
+    });
+    const body = await response.text();
+    return `${api_path}/Images/${body}`
+}
+
 
 export const useFetchCertificates = (id: string) => {
 
